449fefcf60c23938dc742036d8a781a8ad85a72e
Viren <144032304+viren15152@users.noreply.github.com>
2025-01-29 17:11:33 +0000

    Added Villains section to Index.cshtml with distinct styling

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,6 +9,7 @@ namespace Justice_League_Profiles.Pages
         private readonly ILogger<IndexModel> _logger;
 
         public List<Hero> Heroes { get; private set; } = new List<Hero>();
+        public List<Villain> Villains { get; private set; } = new List<Villain>(); // ✅ Added Villains list
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -17,17 +18,32 @@ namespace Justice_League_Profiles.Pages
 
         public void OnGet()
         {
+            // ✅ Heroes Section
             Heroes = new List<Hero>
             {
                 new Hero { Id = 1, Name = "Superman (Kal-EL, Clark Kent)", Description = "The Man of Steel, protector of Earth. Powers: Super-strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor.", ImageUrl = "/images/superman.jpg", DcProfileUrl = "https://www.dc.com/characters/superman" },
-                new Hero { Id = 2, Name = "Batman (Bruce Wayne)", Description = "The Dark Knight, defender of Gotham City. Powers: Batman does not have any metahuman abilities. Instead, he relies on his sharp mind and disciplined body, as well as his extensive combat and detective training. A master of virtually every form of martial arts, a brilliant tactician and a genius-level forensic scientist.", ImageUrl = "/images/batman.jpg", DcProfileUrl = "https://www.dc.com/characters/batman" },
-                new Hero { Id = 3, Name = "Wonder Woman (Diana Prince)", Description = "Princess Diana of Themyscira. Powers: Super strength, invulnerability, flight, combat skill, combat strategy, superhuman agility, healing factor, magic weaponry.", ImageUrl = "/images/wonderwoman.jpg", DcProfileUrl = "https://www.dc.com/characters/wonder-woman" },
-                new Hero { Id = 4, Name = "The Flash (Barry Allen)", Description = "The fastest man alive. Powers: Super speed, intangibility, superhuman agility, time travel, creates and controls lightning, multiversal knowledge", ImageUrl = "/images/flash.jpg", DcProfileUrl = "https://www.dc.com/characters/the-flash" },
-                new Hero { Id = 5, Name = "Aquaman (Arthur Curry)", Description = "The King of Atlantis. Powers: Super strength, durability, control over sea life, exceptional swimming ability, ability to breathe underwater", ImageUrl = "/images/aquaman.jpg", DcProfileUrl = "https://www.dc.com/characters/aquaman" },
-                new Hero { Id = 6, Name = "Green Lantern (Hal Jordan)", Description = "Protector of Sector 2814. Powers: Hard light constructs, instant weaponry, force fields, flight, durability, alien technology", ImageUrl = "/images/greenlantern.jpg", DcProfileUrl = "https://www.dc.com/characters/green-lantern" },
-                new Hero { Id = 7, Name = "Cyborg (Victor Stone)", Description = "Half man, half machine, all hero. Powers: Super strength, advanced technology, instant weaponry, genius-level intellect, control over technology, computer hacking, durability, teleportation", ImageUrl = "/images/cyborg.jpg", DcProfileUrl = "https://www.dc.com/characters/cyborg" },
-                new Hero { Id = 8, Name = "Martian Manhunter (J'onn J'onzz)", Description = "J'onn J'onzz, the last Martian. Powers: Flight, super strength, invulnerability, enhanced speed, shapeshifting, invisibility, telepathy, telekinesis, intelligence, intangibility, optic blasts", ImageUrl = "/images/martianmanhunter.jpg", DcProfileUrl = "https://www.dc.com/characters/martian-manhunter" },   
-                new Hero { Id = 9, Name = "Green Arrow (Oliver Queen)", Description = "Master archer. Powers: Archery, unrivaled aim, exceptional martial artist, inexhaustible wealth. Billionaire Oliver Queen uses both his wealth and his unmatched archery skills as the Justice League's battling bowman, Green Arrow.", ImageUrl = "/images/greenarrow.jpg", DcProfileUrl = "https://www.dc.com/characters/green-arrow" }
+                new Hero { Id = 2, Name = "Batman (Bruce Wayne)", Description = "The Dark Knight, defender of Gotham City.", ImageUrl = "/images/batman.jpg", DcProfileUrl = "https://www.dc.com/characters/batman" },
+                new Hero { Id = 3, Name = "Wonder Woman (Diana Prince)", Description = "Princess Diana of Themyscira.", ImageUrl = "/images/wonderwoman.jpg", DcProfileUrl = "https://www.dc.com/characters/wonder-woman" },
+                new Hero { Id = 4, Name = "The Flash (Barry Allen)", Description = "The fastest man alive.", ImageUrl = "/images/flash.jpg", DcProfileUrl = "https://www.dc.com/characters/the-flash" },
+                new Hero { Id = 5, Name = "Aquaman (Arthur Curry)", Description = "The King of Atlantis.", ImageUrl = "/images/aquaman.jpg", DcProfileUrl = "https://www.dc.com/characters/aquaman" },
+                new Hero { Id = 6, Name = "Green Lantern (Hal Jordan)", Description = "Protector of Sector 2814.", ImageUrl = "/images/greenlantern.jpg", DcProfileUrl = "https://www.dc.com/characters/green-lantern" },
+                new Hero { Id = 7, Name = "Cyborg (Victor Stone)", Description = "Half man, half machine, all hero.", ImageUrl = "/images/cyborg.jpg", DcProfileUrl = "https://www.dc.com/characters/cyborg" },
+                new Hero { Id = 8, Name = "Martian Manhunter (J'onn J'onzz)", Description = "J'onn J'onzz, the last Martian.", ImageUrl = "/images/martianmanhunter.jpg", DcProfileUrl = "https://www.dc.com/characters/martian-manhunter" },   
+                new Hero { Id = 9, Name = "Green Arrow (Oliver Queen)", Description = "Master archer.", ImageUrl = "/images/greenarrow.jpg", DcProfileUrl = "https://www.dc.com/characters/green-arrow" }
+            };
+
+            // ✅ Villains Section
+            Villains = new List<Villain>
+            {
+                new Villain { Id = 1, Name = "Darkseid", Description = "The ruler of Apokolips and a god-like conqueror. Powers: Super strength, invulnerability, genius-level intellect, combat strategy, alien technology, teleportation, energy blasts", ImageUrl = "/images/darkseid.jpg", DcProfileUrl = "https://www.dc.com/characters/darkseid" },
+                new Villain { Id = 2, Name = "The Joker", Description = "Gotham's Clown Prince of Crime. Powers: Complete unpredictability, chaos agent, customized weapons, wields toxin which causes victims to die laughing", ImageUrl = "/images/thejoker.jpg", DcProfileUrl = "https://www.dc.com/characters/joker" },
+                new Villain { Id = 3, Name = "Doomsday", Description = "A weapon crafted in the most extreme conditions imaginable Powers: super strength, invulnerability, super speed, healing factor, superhuman agility, toxikinesis", ImageUrl = "/images/doomsday.jpg", DcProfileUrl = "https://www.dc.com/characters/doomsday" },
+                new Villain { Id = 4, Name = "Reverse-Flash (Eobard Thawne)", Description = "Time-traveling nemesis of The Flash. Powers: Granted with powers and abilities similar to that of the Flash", ImageUrl = "/images/reverseflash.jpg", DcProfileUrl = "https://www.dc.com/characters/reverse-flash" },
+                new Villain { Id = 5, Name = "Black Manta", Description = "Aquaman's greatest enemy is the scourge of the seven seas. Powers: Enhanced strength, energy blasts, skilled fighter, unrivaled swimmer, master swordsman, advanced technology, intelligence.", ImageUrl = "/images/blackmanta.jpg", DcProfileUrl = "https://www.dc.com/characters/black-manta" },
+                new Villain { Id = 6, Name = "Sinestro", Description = "From the greatest Green Lantern to the Corps' greatest enemy. Powers: Wields a yellow power ring that is functionally similar to those of the Green Lanterns, granting him flight, the ability to survive in any environment and the ability to create constructs of any shape and size.", ImageUrl = "/images/sinestro.jpg", DcProfileUrl = "https://www.dc.com/characters/sinestro" },
+                new Villain { Id = 7, Name = "Deathstroke", Description = "Slade Wilson is a mercenary and assassin for hire with no qualms about crossing lines to get the job done Powers: Exceptional martial artist, combat strategy, durability, enhanced strength and speed, weapons expert, superhuman agility, master swordsman", ImageUrl = "/images/deathstroke.jpg", DcProfileUrl = "https://www.dc.com/characters/deathstroke " },
+                new Villain { Id = 8, Name = "Black Adam", Description = "Ancient anti-hero with god-like powers. Powers: Immortality, super strength, super speed, invulnerability, flight, magic, teleportation, healing factor", ImageUrl = "/images/blackadam.jpg", DcProfileUrl = "https://www.dc.com/characters/black-adam" },
+                new Villain { Id = 9, Name = "Brainiac", Description = "Super-intelligent alien conqueror. Powers: Alien technology, genius-level intellect", ImageUrl = "/images/brainiac.jpg", DcProfileUrl = "https://www.dc.com/characters/brainiac" }
             };
         }
     }
@@ -38,9 +54,19 @@ namespace Justice_League_Profiles.Pages
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required string ImageUrl { get; set; }
-        public required string DcProfileUrl { get; set; } // Added this property for the DC Comics website link
+        public required string DcProfileUrl { get; set; }
+    }
+
+    public class Villain //New Villain class added
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string ImageUrl { get; set; }
+        public required string DcProfileUrl { get; set; }
     }
 }
+
 
 
 
